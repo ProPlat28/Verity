34d18246d75a8af09efb62c11b80ec4700161c6d
ProPlat28 <johnwickthebest96@gmail.com>
2026-09-15 19:05:34 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,11 @@ using namespace geode::prelude;
 
 class $modify(MyMenuLayer, MenuLayer) {
 public:
-	void onMoreGames(CCObject*) {
-		CCDirector::sharedDirector()->pushScene(CCTransitionFade::create(0.5f, MyLayer::scene()));
-	}
+    void onMoreGames(CCObject*) {
+        FMODAudioEngine::sharedEngine()->playMusic("Verity.mp3", true, 1.0f, 0);
+
+        CCDirector::sharedDirector()->pushScene(
+            CCTransitionFade::create(0.5f, MyLayer::scene())
+        );
+    }
 };
