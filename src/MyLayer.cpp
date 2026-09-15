c82e5d32f973b67850301e079cd0b2673bb3619b
ProPlat28 <johnwickthebest96@gmail.com>
2026-09-15 17:30:14 +0200

    Update MyLayer.cpp

--- src/MyLayer.cpp
+++ src/MyLayer.cpp
@@ -12,9 +12,17 @@ CCScene* MyLayer::scene() {
 }
 
 bool MyLayer::init() {
-    if (!CCLayer::init()) return false;
+    if (!CCLayer::init())
+        return false;
 
-    void MyLayer::keyBackClicked() {
+    auto audio = FMODAudioEngine::sharedEngine();
+    audio->stopMusic(0);
+    audio->playMusic("Verity.mp3", true, 0.0f, 0);
+
+    return true;
+}
+
+void MyLayer::keyBackClicked() {
     auto audio = FMODAudioEngine::sharedEngine();
     audio->stopMusic(0);
     audio->playMusic("menuLoop.mp3", true, 0.0f, 0);
@@ -23,43 +31,14 @@ bool MyLayer::init() {
         0.5f,
         PopTransition::kPopTransitionFade
     );
-   }
-
-    CCScene* MyLayer::scene() {
-    auto layer = MyLayer::create();
-    auto scene = CCScene::create();
-    scene->addChild(layer);
-
-    return scene;
 }
-
 bool MyLayer::init() {
     if (!CCLayer::init()) return false;
 
-    {
-        auto audio = FMODAudioEngine::sharedEngine();
-        audio->stopMusic(0);
-        audio->playMusic("Verity.mp3", true, 0.0f, 0);
-    }
-
-    return true;
-    }
-
-    void MyLayer::keyBackClicked() {
-        auto audio = FMODAudioEngine::sharedEngine();
-        audio->stopMusic(0);
-        audio->playMusic("menuLoop.mp3", true, 0.0f, 0);
-
-    CCDirector::sharedDirector()->popSceneWithTransition(
-        0.5f,
-        PopTransition::kPopTransitionFade
-        );
-    }
-    
     auto winSize = CCDirector::sharedDirector()->getWinSize();
        
     auto background = createLayerBG();
-    background->setColor({ 0, 88, 220 });
+    background->setColor({ 255, 0, 0 });
     this->addChild(background);
 
     auto menu = CCMenu::create();
@@ -68,7 +47,7 @@ bool MyLayer::init() {
     menu->setAnchorPoint({ 0.f, 0.f });
     this->addChild(menu);
 
-    auto text = CCLabelBMFont::create("Hello, I'm Verity!", "bigFont.fnt");
+    auto text = CCLabelBMFont::create("Hello, world!", "bigFont.fnt");
     text->setPosition({ winSize.width * 0.5f, winSize.height * 0.5f });
     this->addChild(text);
 
@@ -81,4 +60,4 @@ bool MyLayer::init() {
 
 void MyLayer::keyBackClicked() {
     CCDirector::sharedDirector()->popSceneWithTransition(0.5f, PopTransition::kPopTransitionFade);
-};
+}
